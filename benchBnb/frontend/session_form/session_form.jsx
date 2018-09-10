79dec3d06805df62d4f547d6f016f16dbac3646d
lingjie zhang <zhanglingjie16303@gmail.com>
2018-09-10 14:12:30 -0400

    working on session_form

---
+++ benchBnb/frontend/session_form/session_form.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const user = Object.assign({}, this.state);
+    this.props.processForm(user);
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          Welcome to BenchBnB!
+          <br/>
+          <div className="login-form">
+            <br/>
+            <label>Username:
+              <input type="text"
+                value={this.state.username}
+                onChange={this.update('username')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <label>Password:
+              <input type="password"
+                value={this.state.password}
+                onChange={this.update('password')}
+                className="login-input"
+              />
+            </label>
+            <br/>
+            <input className="session-submit" type="submit" value={this.props.formType} />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SessionForm);
